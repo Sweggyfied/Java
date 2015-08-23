8cdccf1f7136af7e769ebb307b719ffe2d955499
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:26:28 -0700

    Getting User Input

---
+++ GettingUserInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;//calls the scanner 
+
+class GettingUserInput {
+	public static void main(String args[]) {
+		Scanner TypeInput = new Scanner(System.in);
+		System.out.println(TypeInput.nextLine()); 
+	}
+}
