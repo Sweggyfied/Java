4598b771ef2733b75ed9762d4fe641feff60c9b0
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:26:06 -0700

    Variables

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+	public static void main(String args[]){
+		double lbs = 3.46;
+		
+		System.out.println("I want ");
+		System.out.print(lbs);
+		System.out.println(" pounds");
+		System.out.print("of sugar cane");
+	}
+}
