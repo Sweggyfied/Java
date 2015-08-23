11965f8b4ac419ebd42246d914ebd44f2ed6b660
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:34:38 -0700

    Math Class Methods

---
+++ MathClassMethods.java
@@ -0,0 +1,5 @@
+class MathClassMethods{
+    public static void main(String[] args){
+        //System.out.println(Math.(abs, ceil, floor, max, min, pow, sqrt all can apply after dot seporator)(1));
+    }
+}
