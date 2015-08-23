64567785db06bf77049e84978b548de0116c3a2e
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:27:33 -0700

    Increment Operators

---
+++ IncrementsOperators.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IncrementsOperators{
+    public static void main(String args[]){
+        Scanner TypedInput = new Scanner(System.in);
+        
+        int tuna = 46;
+        int bass = 26;
+        
+        //*=0    -=0    +=0    /=0
+        
+        System.out.println(tuna++);
+        System.out.println(tuna);        
+
+    }
+}
