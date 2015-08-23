46eec24eee288b5fdc9d77163a920e7e41846d54
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:28:47 -0700

    While Loop

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+class WhileLoop{
+    public static void main(String args[]){
+        int NumTimes = 0;
+        while (NumTimes < 11){
+            System.out.println(NumTimes);
+            NumTimes++;
+        }
+    }
+}
