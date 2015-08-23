32b9a2a20091266b56604cf84311f1efa33383b3
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:27:10 -0700

    Math Operators

---
+++ MathOperators.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class MathOperators {
+	public static void main(String args[]){
+		Scanner TypedInput = new Scanner(System.in);
+		
+		int girls, boys, people;
+		girls = 16;
+		boys = 64;
+		people = girls / boys;
+		System.out.println(people);
+	}
+}
