7ec73e35282e91447a3fa3049bc554dc74031d09
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:28:18 -0700

    Logical Operators

---
+++ LogicalOperators.java
@@ -0,0 +1,13 @@
+public class LogicalOperators{
+    public static void main(String args[]){
+        int kiwis, grapes;
+        kiwis = 18;
+        grapes = 19;
+        
+        if (kiwis > 10 && grapes < 20){
+            System.out.println("You have enough Fruit");
+        }else{
+            System.out.println("You don't have enough fruit");
+        }
+    }
+}
