1436a72546712c52f119613b94434223e289c46c
Sweggyfied <edgar.rivera@techhigh.us>
2015-08-23 15:27:54 -0700

    If Statement

---
+++ IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement{
+    public static void main(String args[]){
+        int test = 26;
+        
+        if (test < 6){    
+            System.out.println("Yes, It is over 6");
+        }else{
+            System.out.println("no, It isn't over 6");    
+        }
+    }
+}
